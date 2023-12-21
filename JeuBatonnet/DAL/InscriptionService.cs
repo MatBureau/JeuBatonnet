4e142df21a73c9358a22d88d4ceded9ea3a52b6e
Francois <81934371+francois-bouloumie@users.noreply.github.com>
2023-12-21 13:44:57 +0100

    Kermit inscription form

---
+++ JeuBatonnet/DAL/InscriptionService.cs
@@ -0,0 +1,22 @@
+﻿using JeuBatonnet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuBatonnet.DAL
+{
+    public static class InscriptionService
+    {
+        public static void InsertUser(Joueur joueurToAdd)
+        {
+            using(JfmContext db = new JfmContext())
+            {
+                db.Joueurs.Add(joueurToAdd);
+                db.SaveChanges();
+            }
+        }
+    }
+}
+    
